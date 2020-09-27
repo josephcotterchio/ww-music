9d952aa3370b7ae675184b1feb7656780e9707b7
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-09-26 20:52:51 -0400

    class

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import './App.css';
+import FavePage from "../FavePage/FavePage";
 import SignUpPage from "../SignUpPage/SignUpPage";
 import LoginPage from "../LoginPage/LoginPage";
 import userService from "../../utils/userService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Welcome to The National and Wine</h2>
-        <a
-          className="App-link"
-          href="https://americanmary.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn about The National
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  state = {
+    play: false,
+  };
+   FavePage = new Audio(this.props.url);
 
-  )
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>"I'm mixing weed with wine" -- The National</h1>
+          <h2>The last music app you'll want</h2>
+          <br />
+          <a
+            className="App-link"
+            href="https://americanmary.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn about The National
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/FavePage/FavePage.jsx
+++ src/pages/FavePage/FavePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
-import { Route, Switch, Link } from "react-router-dom";
 import "./FavePage.css";
 
 const FavePage = (props) => {
